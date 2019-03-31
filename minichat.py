1f3190a236bcd5d6903902aafc5a5e3a78215f06
Gasparsa <34282336+Gasparsa@users.noreply.github.com>
2019-03-30 22:28:27 -0300

    Update minichat.py

--- minichat.py
+++ minichat.py
@@ -3,28 +3,28 @@ import socket
 import sys
 from threading import Thread
 import os
-connections = {}
-def cliente(connection):
-    connection.send(("vc está conectado\nQual seu nome: ").encode())
-    nome = connection.recv(4096).decode('utf-8').strip()
-    connections[connection] = nome
-    connection.send("> ".encode())    
+usuarios = {}
+def cliente(conexao):
+    conexao.send(("vc está conectado\nQual seu nome: ").encode())
+    nome = conexao.recv(4096).decode('utf-8').strip()
+    usuarios[conexao] = nome
+    conexao.send("> ".encode())    
     while True:
-        mensagem = connection.recv(4096).decode('utf-8').strip()
+        mensagem = conexao.recv(4096).decode('utf-8').strip()
         if mensagem == ":bye": 
-            connection.send("saiu.\n".encode())
+            conexao.send("saiu.\n".encode())
             print("%s saiu."% nome)
-            connection.close()
-            del connections[connection]
-            for con in connections:
-                con.send(("%s saiu\n"%nome).encode())
+            conexao.close()
+            del usuarios[conexao]
+            for conectado in usuarios:
+                conectado.send(("%s saiu\n"%nome).encode())
             exit()            
         if mensagem:
             print("%s disse: %s"%(nome, mensagem))
-            for con in connections:
-                if not (con == connection):
-                    con.send(("%s disse: %s \n> "%(nome, mensagem)).encode())
-                else: con.send("> ".encode())
+            for conectado in usuarios:
+                if not (conectado == conexao):
+                    conectado.send(("%s disse: %s \n> "%(nome, mensagem)).encode())
+                else: conectado.send("> ".encode())
 porta = int(sys.argv[1] if len(sys.argv) > 1 else 9090)
 host_ip = socket.gethostbyname(socket.gethostname())
 with socket.socket() as s:
@@ -33,6 +33,6 @@ with socket.socket() as s:
     print("Conectar no ip %s pela porta %s"%(host_ip, porta))
     
     while True:
-        connection, address = s.accept()
-        print("%s o ip se conectou"%address[0])
-        Thread(target=cliente, args=(connection,)).start()
+        conexao, address = s.accept()
+        print("O ip %s se conectou"%address[0])
+        Thread(target=cliente, args=(conexao,)).start()
